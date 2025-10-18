b4bf7407b8a93284c5a73c6de845182987e3ada8
7Spade <7s.i@pm.me>
2025-10-18 11:51:04 +0800

    feat: Update production environment configuration and add file replacements for optimized builds

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "YOUR_API_KEY", // e.g. AIzaSy...
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID",
-    measurementId: "YOUR_MEASUREMENT_ID"
+    apiKey: "AIzaSyCJ-eayGjJwBKsNIh3oEAG2GjbfTrvAMEI",
+    authDomain: "elite-chiller-455712-c4.firebaseapp.com",
+    projectId: "elite-chiller-455712-c4",
+    storageBucket: "elite-chiller-455712-c4.firebasestorage.app",
+    messagingSenderId: "7807661688",
+    appId: "1:7807661688:web:29a373231a5fa5ae1d1f8d",
+    measurementId: "G-YZHBTZSY91"
   }
 };
